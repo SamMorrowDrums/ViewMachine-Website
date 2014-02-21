8ead065ae0d0dfd8ef212353734c07374ec1f0c0
Sam Morrow <info@sammorrowdrums.com>
2014-02-21 21:25:48 +0000

    Created skeleton node app

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-app.set('port', process.env.PORT || 3000);
-app.use(express.static(path.join(__dirname +'/public', 'public')));
-app.listen();
+app.set('title', 'View Machine');
+app.use(express.logger());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'lib' , 'VM', 'dist')));
+app.use(express.static(path.join(__dirname, 'lib' , 'VM', 'dist')));
+app.use(express.static(path.join(__dirname, 'lib' , 'VM3D')));
+app.listen(process.env.PORT || 3000);
