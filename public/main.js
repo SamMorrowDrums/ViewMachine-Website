8fd3488c4fc7377759a08adee821be7d47f16dc8
Sam Morrow <info@sammorrowdrums.com>
2014-02-22 15:34:43 +0000

    Initial homepage complete

--- public/main.js
+++ public/main.js
@@ -1,10 +1,5 @@
 (function ($, VM) {
 	var body = new VM.El('div', {'class': 'body'});
-	$(document).ready(function (){
-		body.draw();
-		setTimeout(function (){logo.animate(2,1,0,360,0,0,0,0);}, 300);
-	});
-
 	var header = new VM.El('header');
 	var logo = new VM.Image('images/viewmachine-logo.png', '', {'class': 'logo', title: 'ViewMachine Logo'});
 	var txtLogo = new VM.Image('images/viewmachine-txt.png', '', {'class': 'logotxt'});
@@ -16,6 +11,20 @@
 	body.append(header);
 	var content = new VM.El('div', {'class': 'content center'});
 	body.append(content);
-	content.append(new VM.El('p', {'class': 'center', text: 'Welcome to ViewMachine, a templating engine to make your Javascript come alive. Visit us on '}).append(new VM.El('a', {text: 'Github.', href: 'https://github.com/SamMorrowDrums/ViewMachine/wiki'})));
-	content.append(new VM.El('h3', {text: 'Coming soon...', 'class': 'center'}));
+	content.append(new VM.El('h3', {text: 'Love JS?', 'class': 'center hidden'}).css('opacity', 0));
+	content.append(new VM.El('h3', {text: 'Hate HTML?', 'class': 'center hidden'}));
+	content.append(new VM.El('p', {'class': 'center hidden', text: 'ViewMachine empowers your JS, so HTML is a consequence, not a hindrance.'}));
+	$(document).ready(function (){
+		body.draw();
+		setTimeout(function (){logo.animate(1.5,1,0,360,0,0,0,0);}, 300);
+		setTimeout(function (){ content.children[0].$().animate({'opacity': '+=1'}, 2000); }, 1500);
+		setTimeout(function (){ content.children[1].$().animate({'opacity': '+=1'}, 2000); }, 3000);
+		setTimeout(function (){
+			$('h3').animate({'opacity': '-=1', 'margin-bottom': '-=30px'}, 800);
+			content.children[2].$().animate({'opacity': '+=1'}, 2000);
+			setTimeout(function (){
+				content.append(new VM.El('p', {'class': 'center', text: 'Visit us on '}).append(new VM.El('a', {text: 'Github.', href: 'https://github.com/SamMorrowDrums/ViewMachine/wiki'}) ).css('margin-top', '50px'));
+			}, 3000);
+		}, 5000);
+	});
 }(jQuery, VM));
