cc1d6f3b40e3d3fb496b276be5cc216beaf7dd77
Sam Morrow <info@sammorrowdrums.com>
2014-02-27 02:08:36 +0000

    Added beginning of feeds demo

--- app.js
+++ app.js
@@ -51,5 +51,21 @@ app.get('/views/:view', function (req, res) {
   });
 });
 
+var feeds = require('./lib/VM-Feeds/VM-Feeds-Server');
+
+//Create a feed with name and details (age in ms and length as integer)
+feeds.createFeed('customFeed', {maxAge: 600000, maxLength: 10});
+
+//Next add in template items, as they are created, using ViewMachine Server (yet to be released)
+for (var i = 0; i < 100; i++) {
+feeds.addItem('customFeed', '{"element":"header","children":[{"element":"img","properties":{"src":"","data-img":"http://viewmachine.io/images/viewmachine-logo.png","class":"logo","title":"ViewMachine Logo"}, "src":"http://viewmachine.io/images/viewmachine-logo.png","preload":"","events":[]},{"element":"img","properties":{"src":"","data-img":"http://viewmachine.io/images/viewmachine-txt.png","class":"logotxt"}, "src":"http://viewmachine.io/images/viewmachine-txt.png","preload":""}]} ');
+}
+//Finally, serve your feed
+app.get('/feeds/:feed', function (req, res) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.json(feeds.fetch(req.params.feed));
+});
+
+
 app.listen(port);
 console.log('Listening on port ' +  port);
